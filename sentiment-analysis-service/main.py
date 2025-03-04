12875842828a3fa917ee2c688b8de0918cddcd03
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-03-04 08:54:39 -0300

    feat: add sentiment analysis function and API endpoint

--- sentiment-analysis-service/main.py
+++ sentiment-analysis-service/main.py
@@ -5,3 +5,22 @@ import nltk
 
 nltk.download('punkt')
 app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
+
+def analyze_sentiment(text: str):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity  # type: ignore
+
+    if polarity >= 1:
+        return 'Positive'
+    elif polarity < 0:
+        return 'Negative'
+    else: 
+        return 'Neutral'
+
+@app.post('/analyze')
+def analyze(text_input: TextInput):
+    sentiment = analyze_sentiment(text_input.text)
+    return {'text': text_input.text, 'sentiment': sentiment}
