e48e9457a992dbd5ad1876a4eebcbb58cc4b8fb7
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-03-05 10:14:28 -0300

    feat: add field validator to ensure text is not empty

--- sentiment-analysis-service/main.py
+++ sentiment-analysis-service/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from textblob import TextBlob
 import nltk
 
+# Downloading the punkt tokenizer
 if not nltk.data.find('tokenizers/punkt'):
     nltk.download('punkt')
 
@@ -11,7 +12,14 @@ app = FastAPI()
 class TextInput(BaseModel):
     text: str
 
-def analyze_sentiment(text: str):
+    @field_validator('text')
+    def check_text_not_empty(cls, value):
+        if len(value) == 0:
+            raise ValueError('Text must not be empty')
+        return value
+        
+def analyze_sentiment(text: str) -> str:
+    # Analyzing text sentiment using TextBlob
     blob = TextBlob(text)
     sentiment = blob.sentiment
     polarity = sentiment.polarity #type: ignore
@@ -26,5 +34,6 @@ def analyze_sentiment(text: str):
 
 @app.post('/analyze')
 def analyze(text_input: TextInput):
+    # Endpoint to analyze the sentiment of a given text
     sentiment = analyze_sentiment(text_input.text)
     return {'text': text_input.text, 'sentiment': sentiment}
